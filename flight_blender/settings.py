f45f9fd9382bce066f3860266a6be9afc0129f4a
Peter Munachi <alimuanchipeter@gmail.com>
2026-01-13 21:05:58 +0100

    feat: prod

--- flight_blender/settings.py
+++ flight_blender/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.getenv("STATIC_ROOT", BASE_DIR / "staticfiles")
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 if DEBUG:
